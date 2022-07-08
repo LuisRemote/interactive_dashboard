e7ffd6864e5852c1eb8bca6b1ae6d06f048cb6cf
LuisRemote <ortizangeles.mx@gmail.com>
2022-07-08 13:54:55 -0500

    Building sidebar

---
+++ app.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+st.set_page_config(page_title="Sales Dashboard",
+                    page_icon=":bar_chart:",
+                    layout="wide"
+)
+
+df = pd.read_excel(
+        io='supermarkt_sales.xlsx',
+        engine='openpyxl',
+        sheet_name='Sales',
+        skiprows=3,
+        usecols='B:R',
+        nrows=1000,
+)
+
+st.dataframe(df)
+
+# ------ SIDEBAR ------
+st.sidebar.header("Please Filter Here:")
+city = st.sidebar.multiselect(
+        "Select the City:",
+        options=df["City"].unique(),
+        default=df["City"].unique()
+)
+
+customer_type = st.sidebar.multiselect(
+        "Select the Customer Type:",
+        options=df["Customer_type"].unique(),
+        default=df["Customer_type"].unique()
+)
+
+gender = st.sidebar.multiselect(
+        "Select the Gender:",
+        options=df["Gender"].unique(),
+        default=df["Gender"].unique()
+)
