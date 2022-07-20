1406f28439b45e781baf3677f39974dcf0be0445
LuisRemote <ortizangeles.mx@gmail.com>
2022-07-20 18:01:09 -0500

    sales by product line chart added

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+
 st.set_page_config(page_title="Sales Dashboard",
                     page_icon=":bar_chart:",
                     layout="wide"
@@ -16,7 +17,6 @@ df = pd.read_excel(
         nrows=1000,
 )
 
-st.dataframe(df)
 
 # ------ SIDEBAR ------
 st.sidebar.header("Please Filter Here:")
@@ -37,3 +37,67 @@ gender = st.sidebar.multiselect(
         options=df["Gender"].unique(),
         default=df["Gender"].unique()
 )
+
+df_selection = df.query(
+        "City == @city & Customer_type == @customer_type & Gender == @gender"
+)
+
+
+# ---- MAINPAGE ----
+st.title(":bar_chart: Sales Dashboard")
+st.markdown("##")
+
+
+# Top KPI's
+total_sales = int(df_selection["Total"].sum())
+average_rating = round(df_selection["Rating"].mean(), 1)
+star_rating = ":star:" * int(round(average_rating, 0))
+average_sale_by_transaction = round(df_selection["Total"].mean(), 2)
+
+left_column, middle_column, right_column = st.columns(3)
+
+with left_column:
+    st.subheader("Total Sales:")
+    st.subheader(f"US $ {total_sales:,}")
+with middle_column:
+    st.subheader("Average Rating:")
+    st.subheader(f"{average_rating} {star_rating}")
+with right_column:
+    st.subheader("Average Sales Per Transaction:")
+    st.subheader(f"US $ {average_sale_by_transaction}")
+
+st.markdown("---")
+
+
+# Sales by product line [Bar chart]
+sales_by_product_line = (
+        df_selection.groupby(by=["Product line"]).sum()[["Total"]].sort_values(by="Total")
+)
+
+fig_product_sales = px.bar(
+        sales_by_product_line,
+        x="Total",
+        y=sales_by_product_line.index,
+        orientation="h",
+        title="<b>Sales by Product Line</b>",
+        color_discrete_sequence=["#0083B8"] * len(sales_by_product_line),
+        template="plotly_white",
+)
+
+st.plotly_chart(fig_product_sales)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
